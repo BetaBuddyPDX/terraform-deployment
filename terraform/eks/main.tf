cf282ab784df93cb126142bd6e814da406caa0e5
hanfangl <hanfangl@amazon.com>
2025-10-31 13:53:00 -0700

    fix

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -17,7 +17,7 @@ variable "deployment_timestamp" {
 resource "aws_kinesis_stream" "apm_test_stream-gitlab" {
   #checkov:skip=CKV_AWS_43:demo only, not encryption is needed
   #checkov:skip=CKV_AWS_185:demo only, not encryption is needed
-  name             = "apm_test2-gitlab"
+  name             = "apm_test2-github-gitlab"
   shard_count      = 2
   
   tags = local.common_tags
@@ -25,7 +25,7 @@ resource "aws_kinesis_stream" "apm_test_stream-gitlab" {
 
 resource "aws_sqs_queue" "apm_test_queue_-gitlab" {
   #checkov:skip=CKV_AWS_27:demo only, not encryption is needed
-  name                      = "apm_test2_-gitlab"
+  name                      = "apm_test2_github-gitlab"
   delay_seconds             = 100
   max_message_size          = 2048
   message_retention_seconds = 86400
@@ -38,7 +38,7 @@ resource "aws_dynamodb_table" "test_2_table-gitlab" {
   #checkov:skip=CKV2_AWS_16:demo only, autoscaling is not needed
   #checkov:skip=CKV_AWS_119:demo only, no encryption is needed
 
-  name           = "test3newtable-gitlab"
+  name           = "test3newtable-github-gitlab"
   billing_mode   = "PROVISIONED"
   read_capacity  = 2
   write_capacity = 5
@@ -64,7 +64,7 @@ resource "aws_dynamodb_table" "test_2_table-gitlab" {
 module "iam_role_inline_policy-gitlab" {
   source = "terraform-aws-modules/iam/aws//modules/iam-role"
 
-  name = "test-role-module-inline-policy-gitlab"
+  name = "test-role-module-inline-policy-github-gitlab"
 
   create_instance_profile = true
 
